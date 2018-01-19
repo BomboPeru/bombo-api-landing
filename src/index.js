b52040e80ea0d3c0efe83ab02097125685a99ada
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2018-01-19 00:26:41 -0500

    Final format

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-import './style';
-import App from './components/app';
+import { h, Component } from 'preact';
+import { Router } from 'preact-router';
+import './style'
+import Home from './pages/home'
+// import Home from 'async!../routes/home';
+// import Profile from 'async!../routes/profile';
+
+class App extends Component {
+
+	handleRoute = e => {
+		this.currentUrl = e.url;
+	};
+
+	render() {
+		return (
+			<div id="app">
+				<Router onChange={this.handleRoute}>
+					<Home path="/" />
+
+				</Router>
+			</div>
+		);
+	}
+}
 
 export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import { h, Component } from 'preact';
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <div>
+            <h1>Hello theaare</h1>
+        </div>
+    );
+  }
+}
